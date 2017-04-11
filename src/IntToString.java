927fa0dc6b8b6ca12c3af59d6fbd85fd7377bf59
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-04-11 15:32:34 +0530

    java_assignment_7.1 is done.

---
+++ src/IntToString.java
@@ -0,0 +1,14 @@
+// class created to show conversion of integer variable to string by using inbuilt method toString(). 
+public class IntToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// integer variable declared and initialized.
+		int a =20;
+		
+		//integer a is converted to nemberAsString using toString()method.
+		String numberAsString = Integer.toString(a);
+		System.out.println("Integer a is converted to String as numberAsString :"+numberAsString);
+	}
+
+}
